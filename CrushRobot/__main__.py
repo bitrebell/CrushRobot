07267b7670da2c730d0a7da8c7a9209587eab0a5
ADIL SHIEKH <114225192+Darkranger00@users.noreply.github.com>
2022-11-10 23:53:19 +0530

    Update __main__.py

--- CrushRobot/__main__.py
+++ CrushRobot/__main__.py
@@ -214,8 +214,8 @@ def start(update: Update, context: CallbackContext):
 
         else:
             first_name = update.effective_user.first_name
-            update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAJYsmLWRvm70cE-mmxSNCovEf4v1ueJAAIcCAACbMK4VuL4EmZEkq8WKQQ"
+            update.effective_message.reply_text(
+                "Hiii welcome Thank for Using The bot"
             )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
