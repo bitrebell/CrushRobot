ea04186f4b99635a4af61990ffd706794591146b
ADIL SHIEKH <114225192+Darkranger00@users.noreply.github.com>
2022-11-10 21:26:44 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Fallen Robot
-:: Enables virtual env mode and then starts Fallen
-env\scripts\activate.bat && py -m FallenRobot
+TITLE Crush Robot
+:: Enables virtual env mode and then starts Crush
+env\scripts\activate.bat && py -m CrushRobot
