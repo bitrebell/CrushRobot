a75a39b4306abb5e6897e7de3b3e38694e1d999e
ADIL SHIEKH <114225192+Darkranger00@users.noreply.github.com>
2022-11-26 22:33:01 +0530

    Create sample_config.py

---
+++ sample_config.py
@@ -0,0 +1,55 @@
+
+    API_ID = 3895734
+    API_HASH = "8ddb546a88c22e086b7735594ebc4be8"
+    TOKEN = "5455938899:AAHKhuGSCL1aTCo1lbOzSXV7adrDLFb3Qz0"
+    OWNER_ID = 5716978554
+    OWNER_USERNAME = "CrushAssistant"
+    SUPPORT_CHAT = "CrushAssistant"
+    JOIN_LOGGER = (-1001306127048)
+    EVENT_LOGS = (-1001677106070)
+
+    SQLALCHEMY_DATABASE_URI = ""
+    MONGO_DB_URI = "mongodb+srv://adil00:12345@cluster0.fyt5yoa.mongodb.net/?retryWrites=true&w=majority"
+    LOAD = []
+    NO_LOAD = ["rss"]
+    WEBHOOK = False
+    INFOPIC = True
+    URL = None
+
+    # OPTIONAL
+    ##List of id's -  (not usernames) for users which have sudo access to the bot.
+    DRAGONS = 5716978554
+    ##List of id's - (not usernames) for developers who will have the same perms as the owner
+    DEV_USERS = 5716978554
+    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    DEMONS = 5716978554
+    # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    TIGERS = 5716978554
+    WOLVES = 5716978554
+
+    DONATION_LINK = "https://t.me/aadillllll"
+    CERT_PATH = None
+    PORT = 5000
+    DEL_CMDS = True
+    STRICT_GBAN = True
+    WORKERS = (
+        8  # Number of subthreads to use. Set as number of threads your processor uses
+    )
+    BAN_STICKER = (
+        "CAACAgUAAxkBAAEDafNhq5Z0DegqVzauwSighMw5cPWp8QACVgQAAuUG0FRXfCEuBziNzCIE"
+    )
+    ALLOW_EXCL = True
+    CASH_API_KEY = "SHOY214OCQ32QEDS"
+    TIME_API_KEY = "ALX2ISMBZXZX"
+    BL_CHATS = []
+    SPAMMERS = None
+    ALLOW_CHATS = True
+    START_IMG = "https://te.legra.ph/file/c899d55728cee2e8fe344.jpg"
+    HEROKU_API_KEY = None
+    HEROKU_APP_NAME = None
+    TEMP_DOWNLOAD_DIRECTORY = "./"
+    ARQ_API_KEY = "LJMETG-DPHBCX-DGHJCD-TMFIGB-ARQ"
+    ARQ_API_URL = "https://arq.hamker.in"
+    ALLOW_EXCL = None
+    BL_CHATS = []  # List of groups that you want blacklisted.
+    SPAMMERS = None
